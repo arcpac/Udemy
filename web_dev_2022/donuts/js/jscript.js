cdd00f94ae874bbab4a9ca37473f9488a9945458
antonraphaelcaballes <antonraphaelcaballes@gmail.com>
2022-07-11 18:26:35 +1000

    add changes whatever it is

---
+++ web_dev_2022/donuts/js/jscript.js
@@ -0,0 +1,19 @@
+// Variable
+var name = 'ANTON';
+console.log(name);
+
+var age = 10;
+console.log('The your age is ' + age);
+var age = 107;
+console.log('The your age is ' + age);
+
+let savings = 100;
+console.log('Savings is ' + savings);
+console.log(savings);
+savings = 500;
+console.log(savings);
+
+const FirstName = 'Anton';
+console.log(FirstName);
+FirstName = 'sdf';
+console.log(FirstName);
